aa2e6752a9e6263bd5ba1caadc3e27fd0cbacc1b
Marc Meumann <marc@steema.com>
2016-03-02 13:01:19 +0100

    mods for new file structure

--- minify.bat
+++ minify.bat
@@ -1,6 +1,6 @@
 rem DO NOT FORCE PATH: set path=C:\Program Files (x86)\Java\jre7\bin;c:\windows
 
-java -jar ..\closure\compiler.jar --js teechart.js --js_output_file teechart-min.js --create_source_map ./teechart-min.js.map --source_map_format=V3
+java -jar ..\misc\closure\compiler.jar --js teechart.js --js_output_file teechart-min.js --create_source_map ./teechart-min.js.map --source_map_format=V3
 
 gzip\gzip -9 <teechart-min.js >teechart.js.gz
 
@@ -8,5 +8,4 @@ mkdir minified
 copy teechart-min.js minified\teechart.js
 copy date.format.js minified\date.format.js
 copy teechart-table.js minified\teechart-table.js
-
 
