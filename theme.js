401d74cc3ec3e3c73addc61e05b53107edc27f8c
Yeray Alonso <yeray@steema.com>
2016-07-29 16:34:07 +0200

    Cleaning

--- theme.js
+++ theme.js
@@ -4,7 +4,7 @@ function changeTheme(chart, aTheme)
    
    for (var i = 0; i < chart.series.count(); i++)
    {
-     if (chart.series.items[i].pointer.format != null)
+     if ((chart.series.items[i].pointer) && (chart.series.items[i].pointer.format))
        chart.series.items[i].pointer.format.stroke.fill = "white";
    }
 }
