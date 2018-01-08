2297c87c67b38563a673a36bc84c91d329ba43d2
Sergi Puigmitja Ruiz <sergi@steema.com>
2018-01-08 11:24:19 +0100

    v2.2 minified

--- minified/teechart-maps.js
+++ minified/teechart-maps.js
@@ -1,7 +1,7 @@
 /*
  TeeChart(tm) for JavaScript(tm)
  @fileOverview TeeChart for JavaScript(tm)
- v2.1 Oct 2017
+ v2.2 Jan 2018
  Copyright(c) 2012-2017 by Steema Software SL. All Rights Reserved.
  http://www.steema.com
 
